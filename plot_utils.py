1eab0fe267c425a6ed1e5db9211b515778572f4e
Wonseok Jeon <jeonwonseok0125@gmail.com>
2018-10-31 01:58:04 +0900

    Final version.

--- plot_utils.py
+++ plot_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 def make_squares(images, nr_images_per_side):
     images_to_plot = np.concatenate(
         [np.concatenate([images[j*nr_images_per_side+i].reshape((28,28)) for i in range(0,nr_images_per_side)],
@@ -9,10 +10,13 @@ def make_squares(images, nr_images_per_side):
         axis=0)
     return images_to_plot
 
-def plot_squares(originals, reconstructs, nr_images_per_side):
+
+def plot_squares(originals, reconstructs, generations, nr_images_per_side):
     originals_square = make_squares(originals, nr_images_per_side)
     plt.imsave('./results/original.png', originals_square, cmap='viridis')
     reconstructs_square = make_squares(reconstructs, nr_images_per_side)
     plt.imsave('./results/recons.png', reconstructs_square, cmap='viridis')
     combined = np.concatenate([originals_square, reconstructs_square], axis=1)
     plt.imsave('./results/combined.png', combined, cmap='viridis')
+    generations_square = make_squares(generations, nr_images_per_side)
+    plt.imsave('./results/generations.png', generations_square, cmap='viridis')
--- vae_gumbel_softmax.py
+++ vae_gumbel_softmax.py
@@ -13,22 +13,21 @@ from tensorflow_probability.python.distributions import Categorical
 # Define Directory Parameters
 flags = tf.app.flags
 flags.DEFINE_string('data_dir', os.getcwd() + '/data/', 'Directory for data')
-flags.DEFINE_string('log_dir', os.getcwd() + '/log/', 'Directory for logs')
 flags.DEFINE_string('results_dir', os.getcwd() + '/results/', 'Directory for results')
 
 # Define Model Parameters
 flags.DEFINE_integer('batch_size', 100, 'Minibatch size')
-flags.DEFINE_integer('num_iters', 50000, 'Number of iterations')
+flags.DEFINE_integer('num_iters', 200000, 'Number of iterations')
 flags.DEFINE_float('learning_rate', 0.001, 'Learning rate')
 flags.DEFINE_integer('num_classes', 10, 'Number of classes')
-flags.DEFINE_integer('num_cat_dists', 20, 'Number of categorical distributions') # num_cat_dists//num_calsses
+flags.DEFINE_integer('num_cat_dists', 20, 'Number of categorical distributions')
 flags.DEFINE_float('min_temp', 0.5, 'Minimum temperature')
-flags.DEFINE_float('anneal_rate', 0.0001, 'Anneal rate')
+flags.DEFINE_float('anneal_rate', 0.00001, 'Anneal rate')
 
 FLAGS = flags.FLAGS
 
 
-def gumbel_softmax(logits, temperature, hard=False):
+def gumbel_softmax(logits, temperature, hard=True):
     gumbel = Gumbel(loc=0., scale=1.)
     # NOTE: softmax over axis=2, which corresponds to the class dimension.
     y = tf.nn.softmax((logits + gumbel.sample(tf.shape(logits))) / temperature)
@@ -87,11 +86,11 @@ def train():
     with tf.variable_scope("decoder"):
         p_x_given_z = decoder(z)
 
-#    with tf.variable_scope("decoder", reuse=tf.AUTO_REUSE):
-#        categorical = Categorical(probs=np.ones(FLAGS.num_classes)/FLAGS.num_classes)
-#        z = categorical.sample(sample_shape=[FLAGS.batch_size, FLAGS.num_cat_dists])
-#        z = tf.one_hot(z, depth=FLAGS.num_classes)
-#        p_x_given_z_eval = decoder(z)
+    with tf.variable_scope("decoder", reuse=tf.AUTO_REUSE):
+        categorical = Categorical(probs=np.ones(FLAGS.num_classes)/FLAGS.num_classes)
+        z = categorical.sample(sample_shape=[FLAGS.batch_size, FLAGS.num_cat_dists])
+        z = tf.one_hot(z, depth=FLAGS.num_classes)
+        p_x_given_z_eval = decoder(z)
 
     # Define loss function and train opeator.
     # NOTE: Categorically uniform prior p(z) is assumed.
@@ -104,12 +103,12 @@ def train():
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        temperature = 0.0
+        temperature = 1.0
         for i in tqdm(range(1, FLAGS.num_iters)):
             np_x, np_y = data.train.next_batch(FLAGS.batch_size)
             _, np_loss = sess.run([train_op, loss], {inputs: np_x, tau: temperature})
             if i % 1000 == 0:
-                temperature = np.maximum(FLAGS.min_temp, np.exp(-FLAGS.anneal_rate * i))
+                temperature = np.maximum(FLAGS.min_temp, np.exp(- FLAGS.anneal_rate * i))
                 print('Temperature updated to {}\n'.format(temperature))
             if i % 5000 == 1:
                 print('Iteration {}\nELBO: {}\n'.format(i, -np_loss))
@@ -126,13 +125,10 @@ def train():
 
 
 def main():
-    if tf.gfile.Exists(FLAGS.log_dir):
-        tf.gfile.DeleteRecursively(FLAGS.log_dir)
-    tf.gfile.MakeDirs(FLAGS.log_dir)
     tf.gfile.MakeDirs(FLAGS.data_dir)
     tf.gfile.MakeDirs(FLAGS.results_dir)
     train()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
